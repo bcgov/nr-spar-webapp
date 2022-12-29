b513714f7327e35e88bbd3f2354a7bfb48bd3b76
mgaseta <matheus.gaseta@encora.com>
2022-12-29 14:51:13 -0300

    fix: adjusting card styles file

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles.css';
+import './styles.scss';
 
 import { Tile, OverflowMenu, OverflowMenuItem } from '@carbon/react';
 import * as Icons from '@carbon/icons-react';
