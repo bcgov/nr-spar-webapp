5166a6c27adf928ed6764436f5911ceb9b98fe92
ArthurEncr <arthur.alves@encora.com>
2023-01-11 15:45:46 -0300

    feat: card test created

--- src/__test__/components/Card.test.tsx
+++ src/__test__/components/Card.test.tsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Card from '../../components/Card/index';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Test the card component', () => {
-  it('should render correctly', () => {
+  it('should render correctly with headers', async () => {
     render(
       <Card
         header="Test"
@@ -13,6 +14,24 @@ describe('Test the card component', () => {
       />
     );
 
-    expect(screen.getByText('Test')).toBeInTheDocument();
+    const headers = await screen.findAllByText('Test');
+    expect(headers[0]).toHaveClass('card-title__small');
+    expect(headers[1]).toHaveClass('card-title__large');
+  });
+});
+
+describe('Test when card is highlighted', () => {
+  it('should render card highlighted with different style', async () => {
+    render(
+      <Card
+        header="Test"
+        description="For testing"
+        icon="SoilMoistureField"
+        highlighted
+      />
+    );
+
+    const headers = await screen.findAllByText('Test');
+    expect(headers[0]).toHaveStyle('color: white');
   });
 });
