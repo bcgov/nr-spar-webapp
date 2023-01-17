757ffd4ccb784ba9aa3c05ff8796c928c1b46a2d
Lucas B. Santos <lucasb.santos@encora.com>
2023-01-17 13:55:06 -0300

    feat: create existing seedlot table

---
+++ src/components/ExistingSeedlot/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Row, Column } from '@carbon/react';
+
+import SeedlotTable from '../SeedlotTable';
+import EmptySection from '../EmptySection';
+
+import ExistingSeedlotItems from '../../mock-data/ExistingSeedlotItems';
---
+++ src/mock-data/ExistingSeedlotItems.ts
@@ -0,0 +1,15 @@
+const ExistingSeedlotItems = [
+  {
+    number: 12456,
+    class: 'A',
+    lot_species: 'SX - Spruce hibrid',
+    form_step: 'Collection',
+    status: 4,
+    participants: 'Placeholder',
+    created_at: 'Oct 10, 2021',
+    last_modified: 'Dec 24, 2022',
+    approved_at: '--'
+  }
+];
+
+export default ExistingSeedlotItems;
