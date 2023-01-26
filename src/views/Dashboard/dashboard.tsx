ae4f24d3b243740fc760a2bfc3a911369e8beb14
ArthurEncr <arthur.alves@encora.com>
2023-01-26 16:32:59 -0300

    feat: change theme on profile menu

--- src/views/Dashboard/dashboard.tsx
+++ src/views/Dashboard/dashboard.tsx
@@ -3,26 +3,34 @@ import React from 'react';
 import {
   FlexGrid,
   Row,
-  Stack
+  Stack,
+  Theme
 } from '@carbon/react';
 
 import PageTitle from '../../components/PageTitle';
 import FavoriteActivities from '../../components/FavoriteActivities';
 import RecentActivities from '../../components/RecentActivities';
+import { useThemePreference } from '../../utils/ThemePreference';
 
-const Dashboard = () => (
-  <FlexGrid className="dashboard-page">
-    <Stack gap={7}>
-      <Row>
-        <PageTitle
-          title="Dashboard"
-          subtitle="See your favorite and recent activities inside SPAR system"
-        />
-      </Row>
-      <FavoriteActivities />
-      <RecentActivities />
-    </Stack>
-  </FlexGrid>
-);
+const Dashboard = () => {
+  const { theme } = useThemePreference();
+  return (
+    <Theme theme={theme}>
+      <FlexGrid className="dashboard-page">
+        <Stack gap={7}>
+          <Row>
+            <PageTitle
+              title="Dashboard"
+              subtitle="See your favorite and recent activities inside SPAR system"
+            />
+          </Row>
+          <FavoriteActivities />
+          <RecentActivities />
+        </Stack>
+      </FlexGrid>
+    </Theme>
+
+  );
+};
 
 export default Dashboard;
