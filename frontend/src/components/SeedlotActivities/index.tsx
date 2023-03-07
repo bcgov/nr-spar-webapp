0e665e9ce61c4fcfb34d5fc20d71c63f2cae9205
Zengkai Yu <craig.yu93@gmail.com>
2023-03-07 14:01:44 -0800

    fix: removed seedlot activity card items from the mock server as it does not belong

--- frontend/src/components/SeedlotActivities/index.tsx
+++ frontend/src/components/SeedlotActivities/index.tsx
@@ -4,12 +4,47 @@ import { Row } from '@carbon/react';
 
 import StandardCard from '../Card/StandardCard';
 
-import SeedlotActivitiesCardItems from '../../mock-api/fixtures/SeedlotActivitiesCardItems';
-
 import './styles.scss';
 
 const SeedlotActivities = () => {
-  const cards = SeedlotActivitiesCardItems;
+  const cards = [
+    {
+      id: '1',
+      image: 'Agriculture',
+      header: 'Register an A class seedlot',
+      description:
+        'Register a seedlot which has been collected in an orchard from parent trees',
+      link: '/seedlot/register-a-class',
+      highlighted: false
+    },
+    {
+      id: '2',
+      image: 'Farm_01',
+      header: 'Register a B class seedlot',
+      description:
+        'Register a seedlot which has been collected from a natural stand',
+      link: '#',
+      highlighted: false
+    },
+    {
+      id: '3',
+      image: 'Sprout',
+      header: 'My seedlots',
+      description:
+        'Consult and manage your own seedlots',
+      link: '#',
+      highlighted: false
+    },
+    {
+      id: '4',
+      image: '',
+      header: 'Activity history',
+      description:
+        'Get updates your latest seedlot related activities',
+      link: '#',
+      highlighted: false
+    }
+  ];
 
   return (
     <div className="seedlot-activities">
