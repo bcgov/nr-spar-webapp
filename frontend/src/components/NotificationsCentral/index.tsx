aa9bee80d25e98b70d7a30fd513eb3651c9f7dd4
Wennio Oliveira <wennio.oliveira@encora.com>
2023-03-07 19:22:10 -0300

    feat: Notification page

--- frontend/src/components/NotificationsCentral/index.tsx
+++ frontend/src/components/NotificationsCentral/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import EmptySection from '../EmptySection';
+import './style.scss';
+
+import {
+  Button,
+  Row,
+} from '@carbon/react';
+import { Notification } from '@carbon/icons-react';
 
 const NotificationsCentral = () => (
-  <span> Placeholder </span>
+  <div className="notifications-space">
+
+    <Row>
+      <Button className='btn-viewed' kind="ghost">Mark all as viewed</Button>
+      <Button className='btn-all-notification' kind="ghost">See all notification</Button>  
+    </Row>
+    <hr></hr>
+    <div className="container">
+      <div className="vertical-center">
+        <EmptySection
+          icon="Sun"
+          title="There is no new notifications!"
+          description="You can go to all notifications to see older ones."
+        />
+        <Button 
+          renderIcon={Notification} 
+          size="field"
+        >
+          See all notifications
+        </Button>
+      </div>
+    </div>
+  </div>
 );
 
 export default NotificationsCentral;
