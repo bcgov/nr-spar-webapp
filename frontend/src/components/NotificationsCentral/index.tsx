18747d5e9065a3165384be52ae237142dc02ef0c
Wennio Oliveira <wennio.oliveira@encora.com>
2023-03-10 18:55:16 -0300

    feat: eslint fixes

--- frontend/src/components/NotificationsCentral/index.tsx
+++ frontend/src/components/NotificationsCentral/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import EmptySection from '../EmptySection';
 import './style.scss';
 import {
   Button,
-  Row,
+  Row
 } from '@carbon/react';
 import { Notification } from '@carbon/icons-react';
+import EmptySection from '../EmptySection';
 
 const NotificationsCentral = () => (
   <div className="notifications-space">
     <Row>
-      <Button className='btn-viewed' kind="ghost">Mark all as viewed</Button>
-      <Button className='btn-all-notifications' kind="ghost">See all notifications</Button>  
+      <Button className="btn-viewed" kind="ghost">Mark all as viewed</Button>
+      <Button className="btn-all-notifications" kind="ghost">See all notifications</Button>
     </Row>
-    <hr></hr>
+    <hr />
     <div className="container">
       <div className="vertical-center">
         <EmptySection
@@ -21,8 +21,8 @@ const NotificationsCentral = () => (
           title="There is no new notifications!"
           description="You can go to all notifications to see older ones."
         />
-        <Button 
-          renderIcon={Notification} 
+        <Button
+          renderIcon={Notification}
           size="field"
         >
           See all notifications
