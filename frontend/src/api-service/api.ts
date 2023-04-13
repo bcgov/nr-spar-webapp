89d44e8ca4c5be71652c6bcf7a003dd65f535ad9
Craig Yu <craig.yu93@gmail.com>
2023-04-13 12:42:19 -0700

    fix: updated how headers are set

--- frontend/src/api-service/api.ts
+++ frontend/src/api-service/api.ts
@@ -1,31 +1,32 @@
 import axios from 'axios';
 
-const token = localStorage.getItem('token');
-
-const headers = {
-  Authorization: `Bearer ${token}`
+const getHeader = () => {
+  const token = localStorage.getItem('token');
+  return {
+    Authorization: `Bearer ${token}`
+  };
 };
 
 const api = {
   get: (url: string, params?: object) => axios.get(url, {
-    headers,
+    headers: getHeader(),
     ...params
   }),
 
   post: (url: string, data: any) => axios.post(url, data, {
-    headers
+    headers: getHeader()
   }),
 
   put: (url: string, data: any) => axios.put(url, data, {
-    headers
+    headers: getHeader()
   }),
 
   patch: (url: string, data: any) => axios.patch(url, data, {
-    headers
+    headers: getHeader()
   }),
 
   delete: (url: string) => axios.delete(url, {
-    headers
+    headers: getHeader()
   })
 };
 
