d6af2d6bf48bd0a2ee4e8a5405d20179e64fc5b6
mgaseta <matheus.gaseta@encora.com>
2023-04-15 13:14:25 -0300

    feat: added parent table

--- frontend/src/components/SeedlotRegistrationSteps/ParentTreeStep/ConeAndPollenTab/index.tsx
+++ frontend/src/components/SeedlotRegistrationSteps/ParentTreeStep/ConeAndPollenTab/index.tsx
@@ -1,10 +1,25 @@
+/* eslint-disable react/no-unused-prop-types */
 import React from 'react';
+import { hashObject } from 'react-hash-string';
 import {
   FlexGrid,
   Row,
   Column,
   InlineNotification,
-  TextInput
+  TextInput,
+  Button,
+  DataTable,
+  TableContainer,
+  TableToolbar,
+  TableToolbarContent,
+  TableToolbarMenu,
+  TableToolbarAction,
+  Table,
+  TableHead,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableCell
 } from '@carbon/react';
 
 import Subtitle from '../../../Subtitle';
@@ -13,8 +28,70 @@ import { pageTexts } from '../constants';
 
 import './styles.scss';
 
+type TableHeaders = {
+  title: string;
+}
+
+type TableRows = {
+  id: string;
+}
+
+interface ParentTreeDataTableProps {
+  rows: Array<TableRows>;
+  headers: Array<TableHeaders>;
+}
+
 const ConeAndPollenTab = () => {
-  console.log('test');
+  const parentTreeHeaders:Array<TableHeaders> = [
+    {
+      title: 'Clone number'
+    },
+    {
+      title: 'Cone count'
+    },
+    {
+      title: 'Pollen count'
+    },
+    {
+      title: 'SMP success (%)'
+    }
+  ];
+  const parentTrees:Array<TableRows> = [
+    {
+      id: '1'
+    },
+    {
+      id: '2'
+    },
+    {
+      id: '3'
+    },
+    {
+      id: '4'
+    },
+    {
+      id: '5'
+    },
+    {
+      id: '6'
+    },
+    {
+      id: '7'
+    },
+    {
+      id: '8'
+    },
+    {
+      id: '9'
+    },
+    {
+      id: '10'
+    },
+    {
+      id: '11'
+    }
+  ];
+
   return (
     <FlexGrid className="cone-pollen-tab">
       <Row className="cone-pollen-title-row">
@@ -32,6 +109,63 @@ const ConeAndPollenTab = () => {
           subtitle={pageTexts.coneAndPollen.notification.subtitle}
           title={pageTexts.coneAndPollen.notification.title}
         />
+      </Row>
+      <Row className="cone-pollen-table-row">
+        <DataTable rows={parentTrees} headers={parentTreeHeaders}>
+          {({
+            rows,
+            headers
+          }: ParentTreeDataTableProps) => (
+            <TableContainer
+              title={pageTexts.tabTitles.coneTab}
+              description={pageTexts.coneAndPollen.tableSubtitle}
+            >
+              <TableToolbar>
+                <TableToolbarContent>
+                  <TableToolbarMenu>
+                    <TableToolbarAction onClick={() => alert('Alert 1')}>
+                      Action 1
+                    </TableToolbarAction>
+                    <TableToolbarAction onClick={() => alert('Alert 2')}>
+                      Action 2
+                    </TableToolbarAction>
+                    <TableToolbarAction onClick={() => alert('Alert 3')}>
+                      Action 3
+                    </TableToolbarAction>
+                  </TableToolbarMenu>
+                  <Button
+                    onClick={() => alert('Alert 3')}
+                    size="small"
+                    kind="primary"
+                  >
+                    Add new
+                  </Button>
+                </TableToolbarContent>
+              </TableToolbar>
+              <Table useZebraStyles>
+                <TableHead>
+                  <TableRow>
+                    {headers.map((header, i) => (
+                      <TableHeader key={hashObject(i)}>
+                        {header.title}
+                      </TableHeader>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row, i) => (
+                    <TableRow key={hashObject(i)}>
+                      <TableCell>{row.id}</TableCell>
+                      <TableCell>{row.id}</TableCell>
+                      <TableCell>{row.id}</TableCell>
+                      <TableCell>{row.id}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </DataTable>
       </Row>
       <Row className="cone-pollen-title-row">
         <Column sm={4} md={5} lg={9}>
@@ -79,28 +213,28 @@ const ConeAndPollenTab = () => {
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="populationSize"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.populationSize}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.populationSize}
             readOnly
           />
         </Column>
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="testedParentTree"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.testedParentTree}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.testedParentTree}
             readOnly
           />
         </Column>
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="coancestry"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.coancestry}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.coancestry}
             readOnly
           />
         </Column>
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="smpParents"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.smpParents}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.smpParents}
             readOnly
           />
         </Column>
@@ -109,33 +243,32 @@ const ConeAndPollenTab = () => {
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="populationSize"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.populationSize}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.populationSize}
             readOnly
           />
         </Column>
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="testedParentTree"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.testedParentTree}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.testedParentTree}
             readOnly
           />
         </Column>
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="coancestry"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.coancestry}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.coancestry}
             readOnly
           />
         </Column>
         <Column sm={2} md={4} lg={4}>
           <TextInput
             id="smpParents"
-            labelText={pageTexts.coneAndPollen.geneticWorth.fieldLabels.smpParents}
+            labelText={pageTexts.coneAndPollen.geneticWorth.defaultFieldsLabels.smpParents}
             readOnly
           />
         </Column>
       </Row>
-
     </FlexGrid>
   );
 };
--- frontend/src/components/SeedlotRegistrationSteps/ParentTreeStep/constants.ts
+++ frontend/src/components/SeedlotRegistrationSteps/ParentTreeStep/constants.ts
@@ -8,6 +8,7 @@ export const pageTexts = {
   },
   coneAndPollen: {
     subtitle: 'Enter the cone and pollen count manually or upload a spreadsheet file with the template for the cone and pollen count table. Remember to keep your orchard updated, you can go to orchard\'s management page to edit the listed parent trees in your orchard.',
+    tableSubtitle: 'Enter the estimative of cone and pollen count for the orchard\'s seedlot (*required)',
     notification: {
       title: 'Upload spreadsheet to table',
       subtitle: 'You can import one spreadsheet file for the cone and pollen count table with the data you want to use. For further guidance on how to organize the data, do use the SPAR\'s spreadsheet template.',
@@ -27,13 +28,41 @@ export const pageTexts = {
     geneticWorth: {
       title: 'Genetic worth and diversity',
       subtitle: 'Check the genetic worth and diversity of your seedlot',
-      fieldLabels: {
+      defaultFieldsLabels: {
         populationSize: 'Effective population size (Ne)',
         testedParentTree: 'Tested parent tree contribution (%)',
         coancestry: 'Coancestry (Sum PiPj * Cij)',
         smpParents: 'Number of SMP parents from outside'
+      },
+      specificFieldsLabels: {
+        ad: 'Genetic worth AD',
+        dfs: 'Genetic worth DFS',
+        dfu: 'Genetic worth DFU',
+        dfw: 'Genetic worth DFW',
+        dsb: 'Genetic worth DSB',
+        dsc: 'Genetic worth DSC',
+        gvo: 'Genetic worth GVO',
+        wdu: 'Genetic worth WDU',
+        wwd: 'Genetic worth WWD',
+        dsg: 'Genetic worth DSG',
+        iws: 'Genetic worth IWS',
+        wve: 'Genetic worth WVE'
       }
     }
   }
 };
 
+export const geneticTraits = [
+  'ad',
+  'dfs',
+  'dfu',
+  'dfw',
+  'dsb',
+  'dsc',
+  'gvo',
+  'wdu',
+  'wwd',
+  'dsg',
+  'iws',
+  'wve'
+];
