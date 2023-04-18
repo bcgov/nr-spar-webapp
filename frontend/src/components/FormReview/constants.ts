44d998732f4dea27878ca1aa4215d40a3c86f285
ArthurEncr <arthur.alves@encora.com>
2023-04-18 12:13:08 -0300

    feat: constant file created at form review component

---
+++ frontend/src/components/FormReview/constants.ts
@@ -0,0 +1,11 @@
+const inputText = {
+  editButton: {
+    labelText: 'Edit step'
+  },
+  reviewForm: {
+    title: 'Form review',
+    subtitle: 'Review data filled in the form (view-only)'
+  }
+};
+
+export default inputText;
