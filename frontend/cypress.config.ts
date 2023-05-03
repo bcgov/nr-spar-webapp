54bfb9a7836dc782099bcceb03dfa60fb5fb4465
Craig Yu <craig.yu93@gmail.com>
2023-05-02 17:14:40 -0700

    feat: configuring cypress code coverage

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'cypress';
 
+declare const require: any;
+
 export default defineConfig({
   e2e: {
     baseUrl: 'https://nr-spar-webapp-test-frontend.apps.silver.devops.gov.bc.ca/',
@@ -9,9 +11,11 @@ export default defineConfig({
     env: {
       apiUrl: 'https://nrbestapi-test-service-api.apps.silver.devops.gov.bc.ca'
     },
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config);
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
     }
   }
 });
--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -1,4 +1,4 @@
+import '@cypress/code-coverage/support';
 import './commands';
-
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
